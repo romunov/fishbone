d3f31134df6fc2589ac719b8e27b4b0f5f1270d2
Roman Luštrik <romunov@gmail.com>
2017-07-20 17:19:30 +0200

    work in progress

--- sandbox/sandbox.R
+++ sandbox/sandbox.R
@@ -5,44 +5,42 @@ library(gridExtra)
 # devtools::use_package("gridExtra")
 
 # extract data for one sample
-# library(readxl)
-# xy <- read_excel("./data/genotypes_dinalpbear_gatc_notrash.xlsx")
-# xy$Marker <- sprintf("%02d", xy$Marker)
-#
-# m1 <- as.data.frame(xy[(xy$Sample_Name %in% "M0TJT.Q") & (xy$Plate %in% 1:8), ])
-# m1$Run_Name <- NULL
-# m1$Sample_ID <- NULL
-# m1$TagCombo <- NULL
-# m1$old_Allele <- NULL
-# m1$Position <- NULL
-# x <- split(m1, f = list(m1$Marker))
-# names(x) <- paste("m.", names(x), sep = "")
-# me <- list2env(x)
-# save(me, file = "./sandbox/samples.RData")
+library(readxl)
+xy <- read_excel("./data/genotypes_dinalpbear_gatc_notrash.xlsx")
+xy$Marker <- sprintf("%02d", xy$Marker)
+xy$Run_Name <- NULL
+xy$Sample_ID <- NULL
+xy$TagCombo <- NULL
+xy$Position <- NULL
 
-load("./sandbox/samples.RData")
-x <- as.fishbone(me$m.03.1, motif = mymotif)
-attributes(x)
-plot(x)
+m1 <- as.data.frame(xy[(xy$Sample_Name %in% "M0P0K.MM") & (xy$Plate %in% 1:8), ])
+m1$Run_Name <- NULL
+m1$Sample_ID <- NULL
+m1$TagCombo <- NULL
+m1$old_Allele <- NULL
+m1$Position <- NULL
+x <- split(m1, f = list(m1$Marker))
+names(x) <- paste("m.", names(x), sep = "")
+me <- list2env(x)
+# save(me, file = "./sandbox/samples.RData")
 
 tbase <- read.table("sandbox/thresholds.csv", sep = "\t", dec = ",", header = TRUE,
                     colClasses = c("character", "character", "numeric"))
 mymotif <- read.table("sandbox/motifs.csv", sep = "\t", header = TRUE,
                       colClasses = "character")
 
+load("./sandbox/samples.RData")
+x <- as.fishbone(me$m.03.1, motif = mymotif)
+plot(x)
+
 m <- mget(names(me)[grepl("m\\.03", names(me))], envir = me)
 m <- sapply(m, FUN = as.fishbone, motif = mymotif, simplify = FALSE)
 plotRuns(m)
 
-lapply(m, FUN = function(i) {
-  ix <- as.fishbone(i, motif.length = nchar(mymotif[mymotif$locus == unique(i$Marker), "motif"]))
-  callGenotype(ix, tbase = tbase, motif = mymotif)
-})
-
-
-
-
-callGenotype(fb = x, tbase = tbase, motif = mymotif)
+m <- x[[3]]
+ss <- as.fishbone(m[m$Plate == 8, ], motif = mymotif)
+plot(ss)
+callGenotype(fb = ss, tbase = tbase, motif = mymotif)
 
 markers <- c("m.03", "m.06", "m.14", "m.16", "m.17", "m.25", "m.51",
              "m.57", "m.63", "m.64", "m.65", "m.67", "m.68")
