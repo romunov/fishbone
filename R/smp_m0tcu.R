22bd21ff400d91cd156227434a8f0679499d1131
Roman Luštrik <romunov@gmail.com>
2017-08-23 16:40:52 +0200

    added dataset

---
+++ R/smp_m0tcu.R
@@ -0,0 +1,13 @@
+#' @name smp_m0tcu
+#' @title Reads of microsatellite sequences of a non-invasive sample
+#' @description NGS (next generation sequencer) data from a project contains sequences, their read count,
+#' sample name, repeat number (PCR plate), marker (locus) designation, run name (arbitrary), position
+#' (on PCR plate), length of sequence, combination of tags used and of course, the microsatellite sequence
+#' itself.
+#' @docType data
+#' @usage smp_m0tcu
+#' @format a \code{RangedData} instance, 1 row per CpG island.
+#' @source LIFE DinAlpBear project
+#' @keywords dataset
+#' @author Roman Lustrik, Tomaz Skrbinsek, Maja Jelencic, Meta Konec
+NULL
