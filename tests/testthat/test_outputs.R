3dee5da483014921c8b85b7bf06ef3673c0d08e0
Roman Luštrik <romunov@gmail.com>
2017-08-24 17:39:22 +0200

    added more unit testing

--- tests/testthat/test_outputs.R
+++ tests/testthat/test_outputs.R
@@ -1,9 +1,6 @@
-library(fishbone)
-
 context("Check inputs")
 data(smp_m0tcu)
 data(mt)
-
 
 test_that("Attached data appears OK", {
   expect_equal(nrow(smp_m0tcu), 536)
@@ -13,11 +10,41 @@ test_that("Attached data appears OK", {
   expect_is(smp_m0tcu, "data.table")
 })
 
-out03 <- callAllele(smp_m0tcu[smp_m0tcu$Plate == "3" & smp_m0tcu$Marker == "03", ], tbase = mt)
+context("Check allele calling")
+
+fb03 <- smp_m0tcu[smp_m0tcu$Plate == "3" & smp_m0tcu$Marker == "03", ]
+fb06 <- smp_m0tcu[smp_m0tcu$Plate == "3" & smp_m0tcu$Marker == "06", ]
+out03 <- callAllele(fb03, tbase = mt)
+out06 <- callAllele(fb06, tbase = mt)
+
+# check correct flag
+
+outall <- smp_m0tcu[, callAllele(c(.BY, .SD), tbase = mt), by = .(Sample_Name, Marker, Plate)]
+outallclean <- outall[, 4:ncol(outall)]
+
 test_that("Is callAllele performin OK", {
   expect_is(out03, "data.table")
   expect_equal(nrow(out03), 3)
   expect_named(out03, c("Sample_Name", "Plate", "Read_Count", "Marker", "Run_Name",
                                "length", "Position", "called", "flag", "stutter", "Sequence",
                                "TagCombo"))
+  expect_named(outall, c("Sample_Name", "Marker", "Plate", "Sample_Name", "Plate", "Read_Count",
+                         "Marker", "Run_Name", "length", "Position", "called", "flag",
+                         "stutter", "Sequence", "TagCombo"))
+  expect_named(outallclean, c("Sample_Name", "Plate", "Read_Count", "Marker", "Run_Name",
+                              "length", "Position", "called", "flag", "stutter", "Sequence",
+                              "TagCombo"))
+  expect_equal(nrow(outallclean[called == TRUE]), 90)
+  expect_equal(nrow(outallclean[stutter == TRUE]), 90)
+  expect_equal(out06[out06$Read_Count == 96, flag], "L")
+})
+
+fb03[, fbid := 1:.N, by = .(Sample_Name, Plate, Marker)]
+st03 <- findStutter(x = fb03[fb03$length == 63 & fb03$Read_Count > 100, ],
+            fb = fb03,
+            motif = as.character(mt[Marker == "03", "Repeat"]))
+
+test_that("Check if detecting stutter works", {
+  expect_length(st03, 1)
+  expect_equal(st03, 1)
 })
